f580687317a2383123aaeb9d2b97f23a8d8ae218
@rafszul <rafszul@wearetheplaymakers.com>
2014-12-03 21:49:40 +0000

    writing app and hexo blog

---
+++ kreatywa-app/main.js
@@ -0,0 +1,51 @@
+Posts = new Mongo.Collection("posts");
+
+if (Meteor.isServer) {
+  // code to run on server at startup
+  Meteor.methods({
+    addPost: function(title, story, author, date) {
+      Posts.insert({
+        title: title,
+        story:story,
+        author: author,
+        date:date
+      });
+    },
+    deletePost: function(id) {
+      Posts.remove(id);
+    }
+  });
+}
+
+if (Meteor.isClient) {
+  Template.addPost.events({
+    'submit form': function(event, template) {
+      event.preventDefault();
+
+      var post = {
+        title: template.find('.title'),
+        story: template.find('.story'),
+        author: template.find('.author'),
+        date: Date.create().full()
+      };
+
+      Meteor.call('addPost', post.title.value, post.story.value, post.author.value, post.date);
+
+      post.title.value = '';
+      post.story.value = '';
+      post.author.value = '';
+    }
+  });
+
+  Template.postList.helpers({
+    posts: function() {
+      return Posts.find();
+    }
+  });
+
+  Template.postList.events({
+    'click .delete': function() {
+      Meteor.call('deletePost', this._id);
+    }
+  });
+}
